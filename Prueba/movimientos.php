b8660f29950a087be2f236211562528868aac957
mboero9 <boero.martin@gmail.com>
2017-11-29 12:35:17 -0300

    ultimo corregido

--- Prueba/movimientos.php
+++ Prueba/movimientos.php
@@ -1,112 +1,104 @@
-<html>
+<?php
+$db_host = 'localhost'; // Server Name
+$db_user = 'root'; // Username
+$db_pass = 'Micro2017'; // Password
+$db_name = 'sam'; // Database Name
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (!$conn) {
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());	
+}
+$id = $_GET['id'];
+//$sql = "SELECT * FROM accionesdemejora where id = '$id'";
+$sql = "SELECT id, titulo, tipo, origen, area, proceso, descripcion, objetivo, 
+		 observacion, estado, subestado, fecha_validacion FROM accionesdemejora WHERE id='$id'";
+
+$sql2 = mysql_query ("UPDATE `movimientos` SET `movi` = '0' WHERE `movimientos`.`id` = '$id'") ;
+	
+
+$query = mysqli_query($conn, $sql);
+
+$resultado = mysql_query('SELECT id FROM accionesdemejora ORDER BY id DESC LIMIT 1');
+
+
+if (!$query) {
+	die ('SQL Error: ' . mysqli_error($conn));
+}
+
+
+?>
+
+<html lang="en">
 <head>
-
-
-  <title>Carga de Movimiento</title>
+  <title>Carga de Movimientos</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  
-    
 </head>
 <body>
-
 
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Nuevo Movimiento</a>
+      <a class="navbar-brand" href="#">ACCION</a>
     </div>
-    <button class="btn btn-danger navbar-btn" onclick="location.href='principal.php'">Volver</button> 
+    <td><button class="btn btn-danger navbar-btn" onclick="location.href='principal.php'">Volver</button></td> 
+	<td><button class="btn btn-danger navbar-btn" onclick="location.href='movimientos.php'">Cargar Movimiento</button></td>
   </div>
 </nav>
 
-
-	<div class="container">
+<div class="container">
 		
 	<form action="procesomov.php" method="post">
     <div class="form-group">
-     
-<!--	 <label for="accion">Accion:</label>
-      <input type="accion" class="form-control" id="accion" placeholder="Ingresa accion">
-	  
-    </div> -->
+<!--		
 
-	
-<?php
-$enlace = mysql_connect('localhost', 'root', 'Prome2017');
-if (!$enlace) {
-    die('No se pudo conectar: ' . mysql_error());
-}
-if (!mysql_select_db('sam')) {
-    die('No se pudo seleccionar la base de datos: ' . mysql_error());
-}
-$resultado = mysql_query('SELECT id FROM accionesdemejora ORDER BY id DESC LIMIT 1');
-if (!$resultado) {
-    die('No se pudo consultar:' . mysql_error());
-}
 
-?>	
-	
-	
+  </div>
 	
 	<div class="form-group">
-		<label for="tipo">Accion:</label>	
-	</div>
-	
-	<div class="form-group">
-		<label for="tipo">Tipo:</label>
-	  		<select type="text" name="tipo" class="form-control">
-			<option value="Analisis causa raiz">Analisis causa raiz</option> 			
-			<option value="Accion de contingencia">Accion de contingencia</option> 
-			<option value="Accion correctiva">Accion correctiva</option> 
-			<option value="Accion propuesta a realizar">Accion propuesta a realizar</option>
-			<option value="Derivacion">Derivacion</option> 
-			<option value="Accion para verificar">Accion para verificar</option> 
-			<option value="Cierre de la accion">Cierre de la accion</option> 
-			</select>
-    </div>
-	
-	
-		<script>
-		function habilitar(value)
-		{
-			if(value=="Derivacion" || value==true)
-			{
-				// habilitamos
-				document.getElementById("Derivad_a").disabled=false;
-			}else if(value=="2" || value==false){
-				// deshabilitamos
-				document.getElementById("Derivado_a").disabled=true;
-			}
-		}
-	</script>
-		
-	
-    <div class="form-group">
-		<label for="derivado_a">Derivado a:</label>
-		<input type="text" class="form-control" name="derivado_a" placeholder="Ingresa derivacion">
+		<label for="objetivo">Objetivo:</label>
+
     </div>
 	
 	<div class="form-group">
-		<label for="descripcion">Descripcion:</label>
-		<textarea type="text" class="form-control" name="descripcion" placeholder="Ingresa descripcion" cols="50" rows="7"></textarea>
+		<label for="observacion">Observacion:</label>
+    </div>
+	
+	<div class="form-group">
+		<label for="estado">Estado:</label>
+    </div>
+	
+	<div class="form-group">
+		<label for="subestado">Subestado:</label>
     </div>	
 	
-		
 	<div class="form-group">
-			<label for="plazo">Plazo:</label>
-			<input type="date">
+		<label for="archivo_adjunto">Archivo adjunto:</label>
 	</div>
-		
+	
 	<div class="form-group">
-	    <button type="submit" class="btn btn-default">Cargar moviemiento</button>
-  	</div>
+			<label for="fecha_validacion">Fecha de validacion:</label>
+	</div>
+	
+	   </div>
+-->
+		<?php
 
-	
+		while ($row = mysqli_fetch_array($query))
+		{
+			echo '<tr>
+					<td><div class="form-group"><label for="observacion">Accion N°: '.$row['id'].'</div></td>
+					<td><div class="form-group"><label for="tipo">Tipo:</label><select type="text" name="tipo" class="form-control"><option value="Analisis causa raiz">Analisis causa raiz</option><option value="Accion de contingencia">Accion de contingencia</option> <option value="Accion correctiva">Accion correctiva</option><option value="Accion propuesta a realizar">Accion propuesta a realizar</option><option value="Derivacion">Derivacion</option> <option value="Accion para verificar">Accion para verificar</option><option value="Cierre de la accion">Cierre de la accion</option> </select></div></td>
+			        <td><div class="form-group"><label for="derivado_a">Derivado a:</label><input type="text" class="form-control" name="derivado_a" placeholder="Ingresa derivacion"></div></td>
+					<td><div class="form-group"><label for="descripcion">Descripcion:</label><textarea type="text" class="form-control" name="descripcion" placeholder="Ingresa descripcion" cols="50" rows="7"></textarea></div></td>
+					<td><div class="form-group"><label for="plazo">Plazo:</label><input type="date"></div></td>
+					<td><div class="form-group"><button type="submit" class="btn btn-default">Cargar movimiento</button></div></td>
+					</tr>';
+		
+		}?>
 </form>
-	
-</body> 
+</body>
 </html>
