9caee19abd438f4288134bac40b55208d0e4343c
mboero9 <boero.martin@gmail.com>
2018-01-17 15:19:27 -0300

    Add files via upload

--- actualizacion.php
+++ actualizacion.php
@@ -1,4 +1,16 @@
 <?php
+  $page_title = 'Actualiza Accion';
+  require_once('includes/load.php');
+?>
+<?php
+
+ page_require_level(1);
+
+ $all_users = find_all_user();
+?>
+<?php include_once('layouts/header.php'); ?>
+
+<?php
 
 $db_host = 'localhost'; // Server Name
 $db_user = 'root'; // Username
@@ -10,30 +22,10 @@ if (!$conn) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());	
 }
 
-//Update User basic info
-  if(isset($_POST['update'])) {
-    $req_fields = array('estado','subestado','fecha_validacion');
-    validate_fields($req_fields);
-    if(empty($errors)){
-           $name = remove_junk($db->escape($_POST['estado']));
-       $username = remove_junk($db->escape($_POST['subestado']));
-          $fecha_validacion = (int)$db->escape($_POST['fecha_validacion']);
-  
-            $sql = "UPDATE accionesdemejora SET estado ='{$estado}', subestado ='{$subestado}',fecha_validacion='{$fecha_validacion}' WHERE id=('$id')";
-         $result = $db->query($sql);
-          if($result && $db->affected_rows() === 1){
-            $session->msg('s',"Actualizado ");
-            redirect('index.php');
-          } else {
-            $session->msg('d',' Fallo la actualizacion!');
-            redirect('edit_user.php?id='.(int)$e_user['id'], false);
-          }
-    } else {
-      $session->msg("d", $errors);
-      redirect('edit_user.php?id='.(int)$e_user['id'],false);
-    }
-  }
+$id = $_GET['id'];
+
 ?>
+
 
 
 <html lang="en">
@@ -46,14 +38,9 @@ if (!$conn) {
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">ACCION</a>
     </div>
     <td><button class="btn btn-danger navbar-btn" onclick="location.href='index.php'">Volver</button></td> 
-	<td><button class="btn btn-danger navbar-btn" onclick="location.href='movimientos.php'">Cargar Movimiento</button></td>
+
   </div>
 </nav>
 
@@ -67,6 +54,11 @@ table {
     border-collapse: collapse;
     border-spacing: 0;
     font: normal 13px Arial, sans-serif;
+	position: absolute;
+	width: 200px;
+    height: 200px;
+    left: 300px;
+    top: 140px;
 }
 
 
@@ -78,42 +70,50 @@ td {
 }
 .zui-table-vertical tbody td {
     border-top: none;
-    border-bottom: none;
+    border-bottom: none;	
+}
+button {
+	position: absolute;
+    left: 300px;
+    top: 350px;
+
 }
 
-
 </style>
-
+<?php
+(isset($_POST["estado"])) ? $estado = $_POST["estado"] : $estado=1;
+(isset($_POST["suestado"])) ? $subestado = $_POST["subestado"] : $subestado=1;
+?>
 <div class="container">
 
 
-
-
+<form action="procesoactualizacion.php" method="post">
 	<table>
 				
 	<tr><td>Estado</td> <td><select type="text" name="estado" class="form-control">
-		<option value="Abierta">Abierta</option> 
-		<option value="Presentada">Presentada</option> 
-		<option value="Cerrada">Cerrada</option>
+		<option <?php if ($estado == Abierta ) echo 'seleccionar' ; ?>value="Abierta">Abierta</option> 
+		<option <?php if ($estado == Presentada ) echo 'seleccionar' ; ?>value="Presentada">Presentada</option> 
+		<option <?php if ($estado == Cerrada ) echo 'seleccionar' ; ?>value="Cerrada">Cerrada</option>
 		</select></td></tr>
 	<tr><td>Subestado</td> <td><select type="text" name="subestado" class="form-control">
-		<option value="Aprobada">Aprobada</option> 
-		<option value="En ejecucion">En ejecucion</option> 
-		<option value="En estudio">En estudio</option>
-		<option value="Implementada">Implementada</option> 
-		<option value="Rechazada">Rechazada</option> 
-		<option value="Exitosa">Exitosa</option>
-		<option value="Sin exito">Sin exito</option>			
+		<option <?php if ($subestado == Aprobada ) echo 'seleccionar' ; ?>value="Aprobada">Aprobada</option> 
+		<option <?php if ($subestado == Ejecucion ) echo 'seleccionar' ; ?>value="Ejecucion">En ejecucion</option> 
+		<option <?php if ($subestado == Estudio ) echo 'seleccionar' ; ?>value="Estudio">En estudio</option>
+		<option <?php if ($subestado == Implementada ) echo 'seleccionar' ; ?>value="Implementada">Implementada</option> 
+		<option <?php if ($subestado == Rechazada ) echo 'seleccionar' ; ?>value="Rechazada">Rechazada</option> 
+		<option <?php if ($subestado == Exitosa ) echo 'seleccionar' ; ?>value="Exitosa">Exitosa</option>
+		<option <?php if ($subestado == Sin_exito ) echo 'seleccionar' ; ?>value="Sin_exito">Sin exito</option>			
 		</select></td></tr>
 	<tr><td>Fecha de validacion</td><td><input type="date"></td></tr>
 		</tbody>
 		</table>
 		
+</form>
+		
 	<div class="form-group">
-	    <button type="submit" class="btn btn-default">Cargar Actualizacion</button>
+	    <?php echo "<td><a href=\"home.php?id=$row[id]\"></a>";?><button type="submit"  class="btn btn-default">Cargar Actualizacion</button>
   	</div>	
-		
-		
-		
-		
-		
+	
+	
+
+	
