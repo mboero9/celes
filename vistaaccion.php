5bf76e515ffdec1e3301d696d4c74dce3d439566
mboero9 <boero.martin@gmail.com>
2018-01-31 11:44:12 -0300

    Add files via upload

--- vistaaccion.php
+++ vistaaccion.php
@@ -1,5 +1,10 @@
 <?php
-
+  $page_title = 'Movimientos';
+  require_once('includes/load.php');
+  if (!$session->isUserLoggedIn(true)) { redirect('index.php', false);}
+?>
+<?php include_once('layouts/header.php'); ?>
+<?php
 $db_host = 'localhost'; // Server Name
 $db_user = 'root'; // Username
 $db_pass = 'Prome2017'; // Password
@@ -13,13 +18,18 @@ $id = $_GET['id'];
 //$sql = "SELECT * FROM accionesdemejora where id = '$id'";
 $sql = "SELECT id, titulo, tipo, origen, area, proceso, descripcion, objetivo, 
 		 observacion, estado, subestado, fecha_validacion FROM accionesdemejora WHERE id='$id'";
+
 $sql2 =	"UPDATE titulo FROM accionesdemejora WHERE id='$id'";	
+
 $query = mysqli_query($conn, $sql);
+
+
 if (!$query) {
 	die ('SQL Error: ' . mysqli_error($conn));
 }
-?>
 
+
+?>
 
 <html lang="en">
 <head>
@@ -31,45 +41,72 @@ if (!$query) {
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<!--		
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">ACCION</a>
-    </div>
-    <td><button class="btn btn-danger navbar-btn" onclick="location.href='index.php'">Volver</button></td> 
-	<td><button class="btn btn-danger navbar-btn" onclick="location.href='movimientos.php'">Cargar Movimiento</button></td>
-	<td><button class="btn btn-danger navbar-btn" onclick="location.href='actualizacion.php'">Actualizar Estado</button></td> 
+
   </div>
-</nav>
+	
+	<div class="form-group">
+		<label for="objetivo">Objetivo:</label>
 
+    </div>
+	
+	<div class="form-group">
+		<label for="observacion">Observacion:</label>
+    </div>
+	
+	<div class="form-group">
+		<label for="estado">Estado:</label>
+    </div>
+	
+	<div class="form-group">
+		<label for="subestado">Subestado:</label>
+    </div>	
+	
+	<div class="form-group">
+		<label for="archivo_adjunto">Archivo adjunto:</label>
+	</div>
+	
+	<div class="form-group">
+			<label for="fecha_validacion">Fecha de validacion:</label>
+	</div>
+	
+	   </div>
+-->
 
-
-<meta charset="UTF-8">
 <style>
-table {
-    border: solid 1px #DDEEEE;
-    border-collapse: collapse;
-    border-spacing: 0;
-    font: normal 13px Arial, sans-serif;
+.table {
+    border: solid 1px #ff7857;
+    border-spacing: 1;
+    font: normal 18px Arial, sans-serif;
+	width: 100%;
 }
 
 
 td {
-    border: solid 1px #DDEEEE;
+	text-align: left;
+	margin: 0 0px 10px 0;
+    border: solid 2px #ff7857;
     color: #333;
     padding: 10px;
-    text-shadow: 1px 1px 1px #fff;
+    position: left;
 }
+
+tr {
+    text-align: left;
+	border: solid 2px #ff7857;
+    color: #ff7857;
+    padding: 5px;
+    }
+
 .zui-table-vertical tbody td {
-    border-top: none;
-    border-bottom: none;
+    border-top: #ff7857;
+    border-bottom: #ff7857;
 }
 
 
 </style>
 
-<div class="container">
 		
   
 		<?php
@@ -77,31 +114,69 @@ td {
 		{
 			echo '
 			
+			
+<table class="table">
+	<tr>
+		<td style="background-color:#ff7857;color:white;" width="20%"><strong>ACCION</strong></td>
+		<td>'.$row['id'].'</td>
+	</tr>
+	<tr>
+		<td style="background-color:#ff7857;color:white;" width="20%"><strong>TITULO</strong></td>
+		<td>'.$row['titulo'].'</td>
+	</tr>
+	<tr>
+		<td style="background-color:#ff7857;color:white;" width="20%"><strong>TIPO</strong></td>
+		<td>'.$row['tipo'].'</td>
+	</tr>
+	<tr>
+		<td style="background-color:#ff7857;color:white;" width="20%"><strong>ORIGEN</strong></td>
+		<td>'.$row['origen'].'</td>
+	</tr>
+	<tr>
+		<td style="background-color:#ff7857;color:white;" width="20%"><strong>PROCESO</strong></td>
+		<td>'.$row['proceso'].'</td>
+	</tr>
+	<tr>
+		<td style="background-color:#ff7857;color:white;" width="20%"><strong>DESCRIPCION</strong></td>
+		<td>'.$row['descripcion'].'</td>
+	</tr>
+	<tr>
+	<tr>
+		<td style="background-color:#ff7857;color:white;" width="20%"><strong>OBSERVACION</strong></td>
+		<td>'.$row['observacion'].'</td>
+	</tr>
+	<tr>
+		<td style="background-color:#ff7857;color:white;" width="20%"><strong>ESTADO</strong></td>
+		<td>'.$row['estado'].'</td>
+	<tr>
+	<tr>
+		<td style="background-color:#ff7857;color:white;" width="20%"><strong>SUBESTADO</strong></td>
+		<td>'.$row['subestado'].'</td>
+	<tr>
+	<tr>
+		<td style="background-color:#ff7857;color:white;" width="20%"><strong>FECHA DE VALIDACION</strong></td>
+		<td>'.$row['fecha_validacion'].'</td>
+	</tr>
+</table>
 					
-					<table class="table table-striped">
-					<tbody>
-						<tr><td font-weight:bolder >Accion</td><td>'.$row['id'].'</td></tr>
-						<tr><td>Titulo</td><td>'.$row['titulo'].'</td></tr>
-						<tr><td>Tipo</td><td>'.$row['tipo'].'</td></tr>
-						<tr><td>Origen</td><td>'.$row['origen'].'</td></tr>
-						<tr><td>Proceso</td><td>'.$row['proceso'].'</td></tr>
-						<tr><td>Observacion</td><td>'.$row['observacion'].'</td></tr>								
-						<tr><td>Estado</td><td>'.$row['estado'].'</td><tr>	  		
-						<tr><td>Subestado</td><td>'.$row['subestado'].'</td><r>
-						<tr><td>Fecha de validacion</td><td>'.$row['fecha_validacion'].'</td></tr>
-					</tbody>
-					</table>';
-											
 					
+	<div method="post" >
+	  <a href="actualizacion.php?id='.$row['id'].'"> 
+		<button type="submit" class="btn btn-danger navbar-btn" onclick="location.href="actualizacion.php" >Actualizar Estado</button>
+		</a>
+	</div>
+		
+	<div method="post">
+	    <a href="movimientos.php?id='.$row['id'].'"> 
+		<button type="submit" class="btn btn-danger navbar-btn" onclick="location.href="movimientos.php" >Cargar Movimiento</button>
+		</a>
+	</div>';				
 				
 		}?>
 
-</div>
 
 
-	<div method="post" class="container">
-	    <button type="submit" class="btn btn-default" onclick="location.href='actualizacion.php'" >Actualizar Estado</button>
-  	</div>
+
 
 			
 </body>
